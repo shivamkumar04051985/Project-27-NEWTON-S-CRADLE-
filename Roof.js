5f18418fa1fe07e04940a529d394d8161b4083ed
shivamkumar04051985 <78911395+shivamkumar04051985@users.noreply.github.com>
2021-03-24 19:34:46 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,22 @@
+class Roof {
+    constructor(x,y,width,height){
+      
+       this.body = Bodies.rectangle(x,y,width,height,{isStatic:true});
+       this.width = width;
+       this.height = height;
+       World.add(world, this.body);
+    
+    }
+
+    display(){
+        
+        rectMode(CENTER);
+        fill(128,128,128);
+        ellipse(this.body.position.x,this.body.position.y,this.width,this.height,25,25);
+       
+        }
+
+
+
+
+}
